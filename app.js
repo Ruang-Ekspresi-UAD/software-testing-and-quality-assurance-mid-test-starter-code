ab778895b80ffb0cd661dbf356658e42427344f9
egasa21 <esaputra168@gmail.com>
2024-11-10 20:43:46 +0700

    feat: implement cors

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const ArticleService = require('./services/articleService');
 const ArticleController = require('./controllers/articleController');
 const articleRoutes = require('./routes/articleRoutes');
 const PORT = process.env.PORT || 8080;
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Dependency Injection
 const articleRepository = new ArticleRepository();
